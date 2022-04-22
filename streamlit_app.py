227b4cff36cd0b0dfd9279aa7c5f490f3a30f0ed
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2022-04-22 20:38:53 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 from sklearn.model_selection import train_test_split
-#from streamlit_shap import st_shap
+from streamlit_shap import st_shap
 import shap
 
 st.title('Feature importance with streamlit-shap')
