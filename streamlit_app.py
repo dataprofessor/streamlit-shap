424c0caf7a20aeefaa5aa0d75fd611795cad284b
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2022-04-24 16:42:01 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,7 +36,7 @@ with st.expander('About the app'):
                 ''')
 
 st.header('Input data')
-st.dataframe(shap.datasets.adult(display=True))
+st.write( pd.DataFrame.from_record( load_data() ) )
 st.header('SHAP output')
  
 # train XGBoost model
